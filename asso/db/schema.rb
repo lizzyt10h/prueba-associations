5c255246f9821050aae09d68ffed95aca42ff10a
Lizzy <ltenganah@unal.edu.co>
2017-10-16 22:03:55 +0000

    Follow model deleted

--- asso/db/schema.rb
+++ asso/db/schema.rb
@@ -30,13 +30,6 @@ ActiveRecord::Schema.define(version: 20171009192859) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "follows", force: :cascade do |t|
-    t.integer  "follower_id", null: false
-    t.integer  "followee_id", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "mallas", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
